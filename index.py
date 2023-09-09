af08c7b08a9867fcd8e12d8ee51fdc10dc034d9a
DJOUMESSI2004 <wilfridndongmo@gmail.com>
2023-09-09 16:57:21 +0200

    importing modules in python

--- index.py
+++ index.py
@@ -1,2 +1,7 @@
-import my_module as mod
+from my_module import find_index as f, test
+# if i wanna imported all i could place asterix at the place of the objects *
 courses = ['History', 'math', 'Physic', 'CompSci']
+
+index = f(courses, 'math')
+print(index)
+print(test)
